5e45a6ba506a4d17928ec56ac7a73a98c6a6c08c
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-01-06 18:01:51 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 112233
+let accountEmail = "Lalit.gupta@gmail.com"
+var accountPassword = "123456"
+acountCity = "Jaipur"
+
+// accountID = 123123
+accountEmail = "lg@gmail.com"
+accountPassword = "1122"
+acountCity = "Delhi"
+
+
+console.log(accountID)
+
+console.table([accountID, accountEmail, accountPassword, acountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let name = "Lalit";
+let age  = 23;
+
+console.log(age);
+
+console.log(typeof age);
+
+// Number
+// bigInt
+// string
+// boolean
+// null
+// undfined
+// symble
+
+// object
