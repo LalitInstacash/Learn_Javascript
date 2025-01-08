28b994522d85088b9947abb82742cd87aea8c4cc
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-01-08 18:14:09 +0000

    Conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "lalit"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+let isLoggedIn = 0
+// console.log(typeof isLoggedIn);
+// console.log(isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+let someNumber = 22;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
