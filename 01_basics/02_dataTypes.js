0e9e8832f4dd741179bdbc45a9dd2695102f53a6
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-01-08 17:50:17 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,16 +1,26 @@
-let name = "Lalit";
-let age  = 23;
+"use strict";
 
-console.log(age);
+// alert(3+3) //we are using nodejs, not browser
 
-console.log(typeof age);
+console.log(3 +3)
 
-// Number
-// bigInt
-// string
-// boolean
-// null
-// undfined
-// symble
+console.log("Hites")
 
-// object
+let name = "Lalit"
+let age = 33
+let isLogiedIn = false
+
+// number => 2 ki power 53
+// bigint  => for show to much big number
+// string => define text
+// boolean => true/false
+// null => Standalone value
+// undefined = 
+// symble => unique
+
+// Object
+
+console.log(typeof null) //Object
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof true)
