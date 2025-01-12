ccc914a4fcf32bb57e822079c78ca2abd7c3f75f
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-01-12 03:01:00 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,5 +13,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let oneNumber = 34
 let stringOneNumber = String(oneNumber)
-console.log(stringOneNumber);
-console.log(typeof stringOneNumber)
+// console.log(stringOneNumber);
+// console.log(typeof stringOneNumber)
+
+// ============================ operations ================
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(4/2)
+// console.log(3%3)
+
+let str1 = "Hello";
+let str2 = " Lalit"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(typeof ("1" + 2))
+// console.log(1 + "2")
+// console.log(typeof ("2" + 1))
+// console.log("1" + 2 +2)
+// console.log(typeof ("1" + 2 +2))
+// console.log(1 + 2 + "2")
+// console.log(typeof (1 + 2 + "2"))
+
+console.log(+true)
+// console.log(true+) // show error on code 
