7fffbc87dfed3fbae622423e03b3575316eea76e
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-01-15 12:30:58 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "Lalit"
+const gitRepo = 50
+
+// console.log(name + gitRepo)
+console.log(`My name is ${name} and my git repo around in ${gitRepo}`)
+
+const gameName = new String('LalitG')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toLowerCase())
+console.log(gameName.toUpperCase())
