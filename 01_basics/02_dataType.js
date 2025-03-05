a6bbd0bd933c67b5d0f82aec9a8b36d9f5d92d87
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-03-05 18:03:25 +0000

    Data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all js code as newer version
+
+// alert("Hello"); // We are using node js not browser
+
+console.log(3 + 3)
+console.log("Hitesh")
+
+let name = "Hitesh"
+let age = 18
+let isLogedIn = false
+
+// Number  => 2 to power 53
+// bigint => To large number
+// string => "" or in ''
+// boolean => true/false
+// null => standalone value
+// undefined => value not define
+// symble => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof isLogedIn)
