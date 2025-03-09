3e530eaf562b78cfb4e0938c1f664e1850d19d72
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-03-09 13:14:40 +0000

    datatype-summary

---
+++ datatypes_summary.js
@@ -0,0 +1,17 @@
+// Primitive data type
+// 7 types => string, number, boolearn, null, undefined, bigint, symble,
+
+// Reference (Non primitive)
+// types => Array, Objects, Functions
+
+const score = 100 // Number
+const newScore = 100.3 // Number
+const isLogedin = false //Boolearn
+const tempOutside = null //null
+let userEmail //undefined
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id === anotherId)
+
+const bigNumber = 123456734568987654n //use n for bigint
