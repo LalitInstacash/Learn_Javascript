ede49cbb6648f70e5fa5a3719bbf95ff12e0338a
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-03-13 07:34:43 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,12 +1,5 @@
-const name = "Lalit"
-const repoCount = 10
-// console.log(name + ' ' + repoCount)
+let name = "Lalit"
+let repoCount = 10
+console.log(name + ' ' +repoCount)
 
 console.log(`My name is ${name} and my repo count is ${repoCount}`)
-
-const gameName = new String("LalitG")
-console.log(gameName[0])
-console.log(gameName[2])
-
-console.log(gameName.length)
-console.log(gameName.toUpperCase())
