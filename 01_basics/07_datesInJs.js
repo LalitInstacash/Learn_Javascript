3010398bd666b6bcd89d28d2609a78db7b4a1727
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-03-17 17:33:12 +0000

    date in js

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,35 @@
+const myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.getDate())
+// console.log(myDate.getMonth())
+// console.log(myDate.getUTCMonth())
+// console.log(myDate.getFullYear())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toUTCString())
+
+// console.log(typeof myDate);
+
+const newDate = new Date(2025,0,15)
+// console.log(newDate)
+// console.log(newDate.toDateString())
+
+const newDate1 = new Date(2025,0,15,5,4)
+// console.log(newDate1.toLocaleString())
+
+const dateType = new Date("2023-01-17")
+// console.log(dateType.toLocaleString());
+
+const dateType1 = new Date("02-17-2025")
+// console.log(dateType1.toLocaleString());
+
+const myTimestamp = Date.now()
+console.log(myTimestamp);
+
+
