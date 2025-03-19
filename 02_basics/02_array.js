39316a712d36fb5ef587b2fedd036757e0848ae4
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-03-19 07:40:09 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,13 @@
+const marver_heros = ["Thor", "Ironman", "Spiderman"]
+const dc_heros = ["Superman", "Flash", "Batman"]
+
+// marver_heros.push(dc_heros)
+// console.log(marver_heros)
+// console.log(marver_heros[3][0])
+
+
+// marver_heros.concat(dc_heros)
+// console.log(marver_heros)
+
+const allHeros = marver_heros.concat(dc_heros)
+console.log(allHeros)
