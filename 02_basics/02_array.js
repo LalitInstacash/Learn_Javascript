5b4dddfb4af80386922effdd62640b41bb4d5bb4
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-03-19 11:52:43 +0000

    Array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -9,5 +9,23 @@ const dc_heros = ["Superman", "Flash", "Batman"]
 // marver_heros.concat(dc_heros)
 // console.log(marver_heros)
 
-const allHeros = marver_heros.concat(dc_heros)
-console.log(allHeros)
+// const allHeros = marver_heros.concat(dc_heros)
+// console.log(allHeros)
+
+// const allHeros = [...marver_heros, ...dc_heros]
+// console.log(allHeros)
+
+// const anotherArray = [1,2,3,[4,5,6],6,7,[3,4,5,[4,5]]]
+// console.log(anotherArray.flat(Infinity))
+
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "Hitesh"}))
+
+const score1 = 100
+const score2 = 200
+const score3 = 300
+const scoreName = "Lalit"
+
+console.log(Array.of(score1, score2,score3, scoreName))
+
