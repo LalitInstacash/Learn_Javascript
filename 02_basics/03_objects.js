3d9f99b7e56571b2cfbe3f88d49e750626f6a0e0
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-03-21 12:25:14 +0000

    object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,13 +1,26 @@
 // Singleton
 
-// Object literals
+// symble
+const mySym = Symbol("key1")
 
+
+// Object literals
 const jsUser = {
     name: "Lalit",
+    [mySym] : "myKey1",
+    "full name": "Lalit Gupta",
     age : 18,
     location : "Jaipur",
     email : "lalit.gupta@gmial.com",
-    // password = "lg2355",
-    // logedIn = false,
-    // lastLogin = []
+    isLogedIn : false,
+    lastLoginDays : ["Monday","Saturday"]
 }
+
+console.log(jsUser.email)
+console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+console.log(jsUser.mySym)
+console.log(typeof jsUser.mySym);
+
+
+
