4f0fbe6c3845731657047fc72f54187da02c2636
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-03-26 12:18:01 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -5,4 +5,16 @@ function sayMyName(){
     console.log("I");
     console.log("T");
 }
-sayMyName()
+// sayMyName()
+
+// function addTwoNumber (number1,number2){
+//     console.log(number1+number2);
+// }
+function addTwoNumber (number1,number2){
+    let result = number1 + number2
+    console.log("Lalit")
+    return result
+    console.log("Lalit") //Result p bad kuch be print ni hota h
+}
+const result = addTwoNumber(4, 34)
+console.log("Result", result)
