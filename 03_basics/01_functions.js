2593c72169faf36417e7d9a6a6cfbd718be5d145
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-03-27 17:56:42 +0000

    Function, array, and object define all

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -11,10 +11,48 @@ function sayMyName(){
 //     console.log(number1+number2);
 // }
 function addTwoNumber (number1,number2){
-    let result = number1 + number2
-    console.log("Lalit")
-    return result
-    console.log("Lalit") //Result p bad kuch be print ni hota h
+    // let result = number1 + number2
+    // console.log("Lalit")
+    // return result
+    // console.log("Lalit") //Result p bad kuch be print ni hota h
+
+    return number1 + number2
 }
 const result = addTwoNumber(4, 34)
-console.log("Result", result)
+// console.log("Result", result)
+
+function userJustLogedIn(username = "sam"){
+    if(username === undefined){
+        console.log(`Please enter username`)
+        return
+    }
+    return `${username} just loged in`
+}
+// console.log(userJustLogedIn("Lalit"));
+// console.log(userJustLogedIn());
+
+function calculatePrice(var1, var2, ...num){
+    return num
+}
+// console.log(calculatePrice(100, 300, 500,200,600));
+
+const user = {
+    username : "Lalit",
+    price : 199
+}
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and his course price is ${anyObject.price}`);   
+}
+// handleObject(user)
+handleObject({
+    username : "lg",
+    price : 399
+})
+
+const myNewArray = [100,300,200,400]
+
+function returnSecondArray(getArray){
+    return getArray[2]
+}
+// console.log(returnSecondArray(myNewArray));
+console.log(returnSecondArray([100,300,200,400]));
