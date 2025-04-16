16b74696619b9ec6e08a44234368516b7239597c
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-04-16 12:24:00 +0000

    data type

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -1,28 +1,4 @@
-"use strict"; // Treat all code as newer version
+"use strict"; // Treat all js code as newer version
 
-// alert(3+3) // We are useing node js not browser
+// alert(3 +3) // We are using node js not browser 
 
-console.log(3+4)
-console.log("Hello")
-
-// Data types
-// String
-let name = "Lalit" 
-console.log(typeof name)
-// Number
-let age = 18 
-console.log(typeof age)
-//Boolean
-let isLogedIN = true 
-console.log(typeof isLogedIN)
-// Bigint
-let bigNumber = 3434434434343434343434343434 
-// null
-let temptemperature = null;
-console.log(typeof temptemperature)
-// undefined
-let schoolName;
-console.log(typeof schoolName)
-// symble //uniqu
-
-// object
