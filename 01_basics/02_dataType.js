6552a9ddce8feb0c6872a1ed01564f22cca847ac
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-04-17 06:51:30 +0000

    Data type

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -2,3 +2,32 @@
 
 // alert(3 +3) // We are using node js not browser 
 
+console.log(3+3)
+console.log("Lalit")
+
+const name = "Lalit"
+const age = 34
+const isLogedIn = false
+let state; 
+
+// table = > 2 to power 53
+// bigint => big number
+// string => single quote or dubble quote
+// Boolean => true or false
+// null => standalone value
+// undefined => value not defined
+// Symbol => unique
+
+
+// object
+
+console.table([name,age,isLogedIn])
+
+console.log(typeof name)
+console.log(typeof isLogedIn);
+console.log(state);
+console.log(typeof null);
+
+
+
+
