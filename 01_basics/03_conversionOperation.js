d42cb71cc1dc254bd8670fdefcbc29e2eae1c680
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-04-22 17:41:11 +0000

    Conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,54 @@
 const score = "Lalit"
 
-console.log(typeof score)
-console.log(typeof(score)) //Both way to right typeof
+// console.log(typeof score)
+// console.log(typeof(score)) //Both way to right typeof
 
 const valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // Number => "33" = 33
-// Number => "33dd" = 
+// Number => "33dd" = NaN
+//  true => 1; false => 0;
+
+const isLogedIn = "Lalit";
+const boolionIsLogedIn = Boolean(isLogedIn);
+// console.log(boolionIsLogedIn)
+// console.log(typeof boolionIsLogedIn)
+
+// 1 => true; 0 => false
+// ""=> false
+// "Lalit" => true
+
+const sumNumber = 33;
+const stringNumber = String(sumNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+const value = 3
+const negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**4)
+// console.log(2/3)
+// console.log(2%3)
+
+const str1 = "Hello"
+const str2 = " Lalit"
+const str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 +2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
