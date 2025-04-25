046d7245daab76c2f39e916dc0d51d7d351d1d8c
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-04-25 06:08:21 +0000

    Comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,28 +1,28 @@
 // console.log(2 > 1)
-// console.log(2 >= 1)
-// console.log(2 <= 2)
-// console.log(2 <= 0)
-// console.log(2 != 2)
+// console.log(2 >= 3)
+// console.log(3<4)
+// console.log(3 <= 3)
+// console.log(3 !=3)
+// console.log(2 != 1)
+// console.log(3 == 2)
+// console.log(4 == 4)
 
-// console.log("2" >1)
-// console.log("02" > 1)
+// console.log("2" > 1)
+// console.log("2">= 1)
+// console.log("2" == 2)
+// console.log(2 == "2")
+// console.log(4 > "5")
+// console.log(1 < "3")
 
-// console.log(null >0)
-// console.log(null >= 0)
-// console.log(null == 0)
-// console.log(null != 0)
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null < 0);
 
-// console.log(true > 0)
-// console.log(true > 1)
-// console.log(true == 1)
-// console.log(false > 0)
-// console.log(false == 0)
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
 
-// console.log(undefined >0)
-// console.log(undefined < 0)
-// console.log(undefined == 0)
-
-// === check
-console.log("1" === 1)
-console.log(1 === 1)
-console.log("Hello" === "Hello")
+console.log("2"===2)
