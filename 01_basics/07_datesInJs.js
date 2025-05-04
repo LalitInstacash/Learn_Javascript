0f710b616ea35abe4f3f79f621f4f66246f68d96
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-05-04 12:50:03 +0000

    date and times

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -18,7 +18,29 @@ const myDate = new Date();
 // console.log(myCreatedDate.toDateString())
 
 const myCreatedDate1 = new Date(2025,4,4, 16,30)
-console.log(myCreatedDate1.toLocaleString())
+// console.log(myCreatedDate1.toLocaleString())
 
-const myCreatedDate2 = new Date("2023-05-04")
-console.log(myCreatedDate2.toLocaleString());
+// const myCreatedDate2 = new Date("2023-05-04")
+const myCreatedDate2 = new Date("04-05-2025")
+// console.log(myCreatedDate2.toLocaleString());
+
+const myTimeStam = Date.now()
+// console.log(myTimeStam)
+// console.log(myCreatedDate2.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+const newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMilliseconds());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getTime());
+
+newDate.toLocaleString('default',{
+    weekday:'long'
+})
+console.log(newDate);
