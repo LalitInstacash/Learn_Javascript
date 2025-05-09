a81d3ebb21b82e6ca0941a651953ba3584fc209c
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-05-09 17:05:17 +0000

    Array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,31 +1,14 @@
-const marver_heros = ["Thor", "Ironman", "Spiderman"]
-const dc_heros = ["Superman", "Flash", "Batman"]
+const marvel_heros = ["thor", "IronMan", "Spiderman"]
+const dc_heros = ["Superan", "Flash", "Batman" ]
 
-// marver_heros.push(dc_heros)
-// console.log(marver_heros)
-// console.log(marver_heros[3][0])
+// console.log(marvel_heros)
+// console.log(dc_heros)
 
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][0]);
 
-// marver_heros.concat(dc_heros)
-// console.log(marver_heros)
-
-// const allHeros = marver_heros.concat(dc_heros)
-// console.log(allHeros)
-
-// const allHeros = [...marver_heros, ...dc_heros]
-// console.log(allHeros)
-
-// const anotherArray = [1,2,3,[4,5,6],6,7,[3,4,5,[4,5]]]
-// console.log(anotherArray.flat(Infinity))
-
-// console.log(Array.isArray("Hitesh"))
-// console.log(Array.from("Hitesh"))
-// console.log(Array.from({name: "Hitesh"}))
-
-const score1 = 100
-const score2 = 200
-const score3 = 300
-const scoreName = "Lalit"
-
-console.log(Array.of(score1, score2,score3, scoreName))
+const allHeros =  marvel_heros.concat(dc_heros)
+console.log(allHeros);
+console.log(allHeros[4]);
 
