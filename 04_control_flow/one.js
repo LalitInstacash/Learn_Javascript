439c261cf99604e1b60bda35fa1300c59466f601
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-05-26 12:34:25 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,14 @@
+//if
+
+const isUserLogedIn = true
+const temperature = 40
+
+if(temperature <= 40){
+    console.log("Temperature is less the 40");   
+}else{
+    console.log("Tempeature is above 50");
+}
+
+
+
+// <, >, <=, >=, !=, ==, ===, !==
