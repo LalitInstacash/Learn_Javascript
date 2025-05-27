25e31f535b782cacdb8c952d4b5f4f65b754d75a
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-05-27 06:00:59 +0000

    if else if

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,14 +1,46 @@
 //if
 
-const isUserLogedIn = true
-const temperature = 40
+// const isUserLogedIn = true
+// const temperature = 40
 
-if(temperature <= 40){
-    console.log("Temperature is less the 40");   
-}else{
-    console.log("Tempeature is above 50");
-}
-
-
+// if(temperature <= 40){
+//     console.log("Temperature is less the 40");   
+// }else{
+//     console.log("Tempeature is above 50");
+// }
 
 // <, >, <=, >=, !=, ==, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "Fly"
+//     console.log(`user power is ${power}`);
+// }
+// console.log(`user power is ${power}`);
+
+// const balance = 1000
+// if(balance> 500) console.log('Test');
+
+// if(balance < 500){
+//     console.log(`Less then 500`);
+// } else if(balance < 750){
+//     console.log(`Less then 750`);
+// }else if (balance < 900){
+//     console.log(`Less then 900`);
+// }else{
+//     console.log(`Balance less then 1200`);   
+// }
+
+const userLogedIn = true
+const debitCard = true
+const loginFromGoogle = false
+const loginFromGmail = true
+
+if(userLogedIn && debitCard && 2==2){
+    console.log(`Now you buy courses`);
+}
+
+if(loginFromGoogle || loginFromGmail){
+    console.log(`You are logedin`);
+}
