ee17eef66552d38d2da69c11b16108a454f7e548
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-05-27 17:50:28 +0000

    Truthy falsy values terniary operator

--- 04_control_flow/truthy_falsy.js
+++ 04_control_flow/truthy_falsy.js
@@ -12,6 +12,29 @@ if(userEmail){
 // Truthy value
 // "0", "false", " ", [], {}, function(){}
 
-if(userEmail.length === 0){
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
     console.log("Object is empty");
 }
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let null1;
+// null1 = 5 ?? 10
+// null1 = null ?? 10
+// null1 = undefined ?? 15
+null1 = undefined ?? 15 ?? 23
+
+console.log(null1);
+
+// Terniary operator
+// condition > true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("Less then 80") : console.log("More then 80");
