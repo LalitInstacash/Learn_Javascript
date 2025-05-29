f021de4c6a931629b51acda24bcf0bdbdcbd661e
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-05-29 05:30:47 +0000

    white, do white loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// white loop
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`Value is less then ${index}`);
+    index = index+2
+}
+
+let myArray = ['Flash', 'Batman', 'Superman']
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    // score++
+    score = score + 1
+} while (score <= 10);
+
