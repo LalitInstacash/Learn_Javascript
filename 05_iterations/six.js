f25eb309ec3e07db4431d25415ea5ef361d434bb
LalitInstacash <52690516+LalitInstacash@users.noreply.github.com>
2025-06-04 11:51:58 +0000

    for each

---
+++ 05_iterations/six.js
@@ -0,0 +1,24 @@
+// const coading = ["js", "cpp", "ruby", "java", "python"]
+
+// const values = coading.forEach( (item)=> {
+//     console.log(item); 
+// } )
+
+// console.log(values);
+
+
+const myNum = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNum.filter( (num) => num > 4 )
+// const newNums = myNum.filter( (num) => {
+//     return num > 4
+// } )
+
+// for each
+
+const newNums = []
+
+
+console.log(newNums);
+
+
